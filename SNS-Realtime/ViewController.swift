08343e2777b4c62493a6abb4d32e0a01a0baeeda
Icaro <icarolavrador@gmail.com>
2015-10-03 14:11:25 +1300

    Initial Commit

---
+++ SNS-Realtime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SNS-Realtime
+//
+//  Created by Icaro Barreira Lavrador on 3/10/15.
+//  Copyright © 2015 Icaro Barreira Lavrador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
